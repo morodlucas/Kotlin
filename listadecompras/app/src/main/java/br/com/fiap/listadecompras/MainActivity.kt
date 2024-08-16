68695a9858f02f41a931d6be267f41432f037c91
morodlucas <52837306+morodlucas@users.noreply.github.com>
2024-08-15 22:09:47 -0300

    lista de compras 15/08

---
+++ listadecompras/app/src/main/java/br/com/fiap/listadecompras/MainActivity.kt
@@ -0,0 +1,55 @@
+package br.com.fiap.listadecompras
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.fiap.listadecompras.ui.theme.ListaDeComprasTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.RecyclerView
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Lista de Compras"
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val itemsAdapter = ItemsAdapter()
+        recyclerView.adapter = itemsAdapter
+
+        val button = findViewById<Button>(R.id.button)
+        val editText = findViewById<EditText>(R.id.editText)
+
+        button.setOnClickListener {
+            if (editText.text.isEmpty()) {
+                editText.error = "Preencha um valor"
+                return@setOnClickListener
+            }
+
+            val item = ItemModel(
+                name = editText.text.toString(),
+                onRemove = {
+                    itemsAdapter.removeItem(it)
+                }
+            )
+
+            itemsAdapter.addItem(item)
+            editText.text.clear()
+        }
+
+    }
+}
